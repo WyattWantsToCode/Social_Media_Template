074f8ef55a79d8226ee3d9e15bd308b42af3d2a8
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-10-17 15:31:29 -0500

    Added mock Stories

--- lib/story_class.dart
+++ lib/story_class.dart
@@ -3,3 +3,11 @@ class Story {
 
   Story({required this.imageURL});
 }
+
+Story mockStory1 = Story(
+    imageURL:
+        "https://drive.google.com/uc?export=view&id=1inQIHTRRkKWHkkZW89_i_fyXqXcS4_Wn");
+Story mockStory2 = Story(
+    imageURL:
+        "https://drive.google.com/uc?export=view&id=17IFFftlYMui86Xpa8azmAggYrbFpTbNP");
+
