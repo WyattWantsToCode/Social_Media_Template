ef14d8ededc2068cc457039780d623c4bed7e5ac
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-10-17 19:43:29 -0500

    Added CurrentUser class

--- lib/user_class.dart
+++ lib/user_class.dart
@@ -14,16 +14,29 @@ class User {
       this.storyList});
 }
 
+class CurrentUser {
+  User user;
+  List<PostClass> likedPosts;
+  List<User> storiesWatched;
+
+  CurrentUser(
+      {required this.user,
+      required this.likedPosts,
+      required this.storiesWatched});
+}
+
+CurrentUser currentUser = CurrentUser(
+    user: mockUser1,
+    likedPosts: <PostClass>[mockPost1],
+    storiesWatched: <User>[]);
+
 User mockUser1 = User(
   name: "Sammy",
   profilePictureURL:
       "https://drive.google.com/uc?export=view&id=11rSmO36l7o-V2B5ONLjvDS-nCz7cuNeh",
-
 );
 
 User mockUser2 = User(
     name: "Sarah",
     profilePictureURL:
         "https://drive.google.com/uc?export=view&id=1XWCJACYGtADzEWYAJTs484WTOLveesVY");
-
-
