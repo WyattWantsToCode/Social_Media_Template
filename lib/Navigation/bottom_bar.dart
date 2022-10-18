0b698bb6d0bdbdee335534c7cf172434afa65ec1
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-10-17 19:25:20 -0500

    added BottomBar

---
+++ lib/Navigation/bottom_bar.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:social_media_template/colors.dart';
+
+class BottomBar extends StatefulWidget {
+  BottomBar({Key? key}) : super(key: key);
+
+  @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: colorBackground,
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8),
+        child: Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+          Icon(Icons.home_filled, color: colorIcon, size: 32,),
+          Icon(Icons.search, color: colorIcon, size: 32,)
+          
+        ],),
+      ),
+    );
+  }
+}
