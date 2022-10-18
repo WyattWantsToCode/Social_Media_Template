f8e5de7e343bdedbfde4e569afee7e391b00d3e5
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-10-18 10:50:49 -0500

    Added displayName and handle

--- lib/user_class.dart
+++ lib/user_class.dart
@@ -2,13 +2,15 @@ import 'package:social_media_template/post_class.dart';
 import 'package:social_media_template/story_class.dart';
 
 class User {
-  String name;
+  String displayName;
+  String handle;
   String profilePictureURL;
   List<PostClass>? postList;
   List<Story>? storyList;
 
   User(
-      {required this.name,
+      {required this.displayName,
+      required this.handle,
       required this.profilePictureURL,
       this.postList,
       this.storyList});
@@ -31,12 +33,14 @@ CurrentUser currentUser = CurrentUser(
     storiesWatched: <User>[]);
 
 User mockUser1 = User(
-  name: "Sammy",
+  displayName: "Sammy",
+  handle: "sammy",
   profilePictureURL:
       "https://drive.google.com/uc?export=view&id=11rSmO36l7o-V2B5ONLjvDS-nCz7cuNeh",
 );
 
 User mockUser2 = User(
-    name: "Sarah",
+    displayName: "Sarah",
+    handle: "sarah",
     profilePictureURL:
         "https://drive.google.com/uc?export=view&id=1XWCJACYGtADzEWYAJTs484WTOLveesVY");
