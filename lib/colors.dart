fddf86946b064c85b83fe80ca69af4dd260be11c
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-10-17 20:18:26 -0500

    Added colorIcon

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 
 Color colorBackground = Color.fromRGBO(1, 1, 1, 1);
+Color colorIcon = Colors.white;
 TextStyle nameStyle = TextStyle(color: Colors.white, fontSize: 16);
