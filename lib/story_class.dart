4e5b28a319fc35ffd33d7851cb5e1201f9851f13
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-10-19 09:21:29 -0500

    Added story id

--- lib/story_class.dart
+++ lib/story_class.dart
@@ -1,13 +1,15 @@
 class Story {
+  String id;
   String imageURL;
 
-  Story({required this.imageURL});
+  Story({required this.imageURL, required this.id});
 }
 
 Story mockStory1 = Story(
+    id: "qwertyuiop",
     imageURL:
         "https://drive.google.com/uc?export=view&id=1inQIHTRRkKWHkkZW89_i_fyXqXcS4_Wn");
 Story mockStory2 = Story(
+    id: "wertyuiopq",
     imageURL:
         "https://drive.google.com/uc?export=view&id=17IFFftlYMui86Xpa8azmAggYrbFpTbNP");
-
