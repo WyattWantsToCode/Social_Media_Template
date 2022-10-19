573f8a25a321b79e5db0db07df15d075ec22ad33
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-10-19 09:19:11 -0500

    Create PostBody widget

---
+++ lib/Posts/Widgets/body.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+import 'package:social_media_template/Posts/post_class.dart';
+import 'package:social_media_template/storage.dart';
+
+class PostBody extends StatefulWidget {
+  PostBody({
+    Key? key, required this.post
+  }) : super(key: key);
+  PostClass post;
+
+  @override
+  State<PostBody> createState() => _PostBodyState();
+}
+
+class _PostBodyState extends State<PostBody> {
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 500,
+      width: double.infinity,
+      child: ListView.builder(
+        physics: PageScrollPhysics(parent: BouncingScrollPhysics()),
+        scrollDirection: Axis.horizontal,
+        itemCount: widget.post.imageURLs.length,
+        itemBuilder: ((context, index) {
+          return FutureBuilder(
+            future: getPostImageURL(widget.post.imageURLs[index]),
+            builder: ((context, snapshot) {
+              if (snapshot.connectionState == ConnectionState.done) {
+                if (snapshot.hasError) {
+                  return const Center(
+                    child: Text("Error"),
+                  );
+                } else if (snapshot.hasData) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width,
+                    height: double.infinity,
+                    decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image: NetworkImage(snapshot.data.toString()),
+                            fit: BoxFit.cover)),
+                  );
+                }
+              }
+
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }),
+          );
+        }),
+      ),
+    );
+  }
+}
