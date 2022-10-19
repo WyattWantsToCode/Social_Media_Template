ce3af02df10dea2a67c5f1da5ff3aa35f6893733
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-10-19 09:20:56 -0500

    Added likes to mapToPostClass

--- lib/firebase.dart
+++ lib/firebase.dart
@@ -2,7 +2,7 @@ import 'dart:math';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:social_media_template/post_class.dart';
+import 'package:social_media_template/Posts/post_class.dart';
 import 'package:social_media_template/story_class.dart';
 import 'package:social_media_template/user_class.dart';
 
@@ -51,7 +51,7 @@ PostClass mapToPosstClass(Map<String, dynamic> map, String id) {
       ID: id,
       imageURLs: imageURLs,
       description: map["description"],
-      likes: mockPost1.likes,
+      likes: map["likes"],
       user: map["user"]);
 }
 
