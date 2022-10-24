37064b10970ce9bae0a51e247d66b851d68be61e
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-10-24 12:35:56 -0500

    Added timeStamp to PostClass

--- lib/Posts/post_class.dart
+++ lib/Posts/post_class.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:social_media_template/user_class.dart';
 
 class PostClass {
@@ -6,37 +7,48 @@ class PostClass {
   String description;
   int likes;
   String user;
+  Timestamp timestamp;
 
   PostClass(
-      {
-      required this.ID,  
+      {required this.ID,
       required this.imageURLs,
       required this.description,
       required this.likes,
-      required this.user});
+      required this.user,
+      required this.timestamp
+      });
 }
 
 PostClass mockPost1 = PostClass(
-  ID: "qwertyuiop",
+    ID: "qwertyuiop",
     imageURLs: <String>[
       "https://drive.google.com/uc?export=view&id=1wXhVIYQJTxIBaZzT3A4Q2k3Hlokkpcvp",
       "https://drive.google.com/uc?export=view&id=1ayBFm6_RRCCTsMNWMfDzpFZsp5i0VDxO"
     ],
     description: "Loved the mountains this weekend",
     likes: 100,
-    user: "mockUser1");
+    user: "mockUser1",
+    timestamp: Timestamp.now()
+    );
 
 PostClass mockPost2 = PostClass(
-  ID: "wertyuiopq",
+    ID: "wertyuiopq",
     imageURLs: <String>[
       "https://drive.google.com/uc?export=view&id=16EcrrtxJkgZ6F4SMv9AAqwwBaYF1Nj25",
     ],
     description: "I am so thankful for this team... they mean the world to me",
     likes: 1200,
-    user: "mockUser1");
+    user: "mockUser1",
+    timestamp: Timestamp.now()
+    );
 
 PostClass mockPost3 = PostClass(
-  ID: "ertyuiopqw",
-  imageURLs: <String>[
-  "https://drive.google.com/uc?export=view&id=15xSw84vkAGezLXamCZM4oHxDRbGupybu",
-], description: "Food... I am better than you", likes: 2, user: "mockUser2");
+    ID: "ertyuiopqw",
+    imageURLs: <String>[
+      "https://drive.google.com/uc?export=view&id=15xSw84vkAGezLXamCZM4oHxDRbGupybu",
+    ],
+    description: "Food... I am better than you",
+    likes: 2,
+    user: "mockUser2",
+    
+    timestamp: Timestamp.now());
