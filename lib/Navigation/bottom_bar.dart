25db46a17dc4d386068766831d1d707ef5b21bee
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-11-04 12:17:54 -0500

    Changed padding

--- lib/Navigation/bottom_bar.dart
+++ lib/Navigation/bottom_bar.dart
@@ -20,56 +20,53 @@ class _BottomBarState extends State<BottomBar> {
     return Container(
       color: colorBackground,
       width: double.infinity,
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 8),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            TextButton(
+      child: Row(
+        mainAxisSize: MainAxisSize.max,
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          TextButton(
+            onPressed: () {
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: (context) {
+                return HomePage();
+              }));
+            },
+            child: Icon(
+              Icons.home_filled,
+              color: colorIcon,
+              size: 32,
+            ),
+          ),
+          TextButton(
               onPressed: () {
-                Navigator.pushReplacement(context,
-                    MaterialPageRoute(builder: (context) {
-                  return HomePage();
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return CreatePostPage();
                 }));
               },
               child: Icon(
-                Icons.home_filled,
+                Icons.add_box_outlined,
                 color: colorIcon,
                 size: 32,
-              ),
+              )),
+          TextButton(
+            onPressed: () async {
+              User user = await getUserByHandle("sarah");
+              List<PostClass> posts = await getPostFromHandle("sarah");
+              Navigator.pushReplacement(context,
+                  MaterialPageRoute(builder: ((context) {
+                return UserProfilePage(
+                  user: user,
+                  posts: posts,
+                );
+              })));
+            },
+            child: Icon(
+              Icons.person_outline,
+              color: colorIcon,
+              size: 32,
             ),
-            TextButton(
-                onPressed: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return CreatePostPage();
-                  }));
-                },
-                child: Icon(
-                  Icons.add_box_outlined,
-                  color: colorIcon,
-                  size: 32,
-                )),
-            TextButton(
-              onPressed: () async {
-                User user = await getUserByHandle("sarah");
-                List<PostClass> posts = await getPostFromHandle("sarah");
-                Navigator.pushReplacement(context,
-                    MaterialPageRoute(builder: ((context) {
-                  return UserProfilePage(
-                    user: user,
-                    posts: posts,
-                  );
-                })));
-              },
-              child: Icon(
-                Icons.person_outline,
-                color: colorIcon,
-                size: 32,
-              ),
-            ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
