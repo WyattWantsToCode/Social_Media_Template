9f70338878cd0437a22456834615ce2b36cd760d
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-11-04 12:16:15 -0500

    Added styleDateTime textStyle

--- lib/colors.dart
+++ lib/colors.dart
@@ -3,3 +3,4 @@ import 'package:flutter/material.dart';
 Color colorBackground = Color.fromRGBO(1, 1, 1, 1);
 Color colorIcon = Colors.white;
 TextStyle nameStyle = TextStyle(color: Colors.white, fontSize: 16);
+TextStyle styleDateTime = TextStyle(color: Colors.grey, fontSize: 12);
