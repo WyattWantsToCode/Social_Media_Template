ee136c78a13217162ea80697ea1e98e4a20bf6aa
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-11-04 12:17:39 -0500

    Added rainbow circle around story

--- lib/story_section.dart
+++ lib/story_section.dart
@@ -8,25 +8,51 @@ class StorySection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<User> users = <User>[mockUser1, mockUser2];
-    return SizedBox(
-      height: 135,
-      child: ListView.builder(
-          scrollDirection: Axis.horizontal,
-          shrinkWrap: true,
-          itemCount: users.length,
-          itemBuilder: ((context, index) {
-            return StoryButton(
-             user: users[index],
-            );
-            
-          })),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(bottom: 7),
+          child: Container(
+            width: double.infinity,
+            height: .2,
+            color: Colors.grey,
+          ),
+        ),
+        SizedBox(
+          height: 108,
+          child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              shrinkWrap: true,
+              itemCount: users.length,
+              itemBuilder: ((context, index) {
+                return StoryButton(
+                  user: users[index],
+                  scale: 1,
+                );
+              })),
+        ),
+        Container(
+          width: double.infinity,
+          height: .2,
+          color: Colors.grey,
+        ),
+      ],
     );
   }
 }
 
 class StoryButton extends StatefulWidget {
-  StoryButton({Key? key, required this.user}) : super(key: key);
+  StoryButton({
+    Key? key,
+    required this.user,
+    this.onPressedFunction,
+    this.scale = 1,
+  }) : super(key: key);
   User user;
+  Function? onPressedFunction;
+  double scale;
 
   @override
   State<StoryButton> createState() => _StoryButtonState();
@@ -36,21 +62,59 @@ class _StoryButtonState extends State<StoryButton> {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(10.0),
+      padding: const EdgeInsets.only(left: 15),
       child: Column(
         children: [
-          Container(
-            width: 80,
-            height: 80,
-            decoration: BoxDecoration(
-                shape: BoxShape.circle,
-                image: DecorationImage(
-                    image: NetworkImage(widget.user.profilePictureURL),
-                    fit: BoxFit.cover)),
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              Opacity(
+                opacity: .9,
+                child: Container(
+                  width: 80 * widget.scale,
+                  height: 80 * widget.scale,
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        begin: Alignment.topRight,
+                        end: Alignment.bottomLeft,
+                        colors: [
+                          Colors.purple,
+                          Colors.purple,
+                          Colors.pink,
+                          Colors.red,
+                          Colors.orange,
+                          Colors.yellow,
+                        ]),
+                    shape: BoxShape.circle,
+                  ),
+                ),
+              ),
+              Container(
+                width: 76 * widget.scale,
+                height: 76 * widget.scale,
+                decoration: BoxDecoration(
+                  color: colorBackground,
+                  shape: BoxShape.circle,
+                ),
+              ),
+              Container(
+                width: 70 * widget.scale,
+                height: 70 * widget.scale,
+                decoration: BoxDecoration(
+                    border: Border.all(color: Colors.white54, width: .5),
+                    shape: BoxShape.circle,
+                    image: DecorationImage(
+                        image: NetworkImage(widget.user.profilePictureURL),
+                        fit: BoxFit.cover)),
+              ),
+            ],
           ),
           Padding(
             padding: const EdgeInsets.only(top: 5),
-            child: Text(widget.user.displayName, style: nameStyle.apply(fontSizeFactor: .85),),
+            child: Text(
+              widget.user.displayName,
+              style: nameStyle.apply(fontSizeFactor: .75),
+            ),
           )
         ],
       ),
