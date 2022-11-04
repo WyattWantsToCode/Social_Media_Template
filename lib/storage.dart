9f3103940ba22ca7e604a5ac6f3c66db3ba7d228
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-11-04 12:17:17 -0500

    Added image compression

--- lib/storage.dart
+++ lib/storage.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_image_compress/flutter_image_compress.dart';
 
 final storage = FirebaseStorage.instance;
 
@@ -9,6 +11,7 @@ Future<String> getPostImageURL(String string) async {
   var url = await ref.getDownloadURL();
   return url;
 }
+//https://firebasestorage.googleapis.com/v0/b/social-media-template-4f6b1.appspot.com/o/Posts%2F03eed040-522a-11ed-bdbe-afcc2a9f60ed?alt=media&token=cf9421f5-ec10-4830-aca1-82738791de39
 
 Future<String> getProfilePictureURL(String string) async {
   final ref = storage.ref("Profile_Pictures").child(string);
@@ -16,6 +19,12 @@ Future<String> getProfilePictureURL(String string) async {
   return url;
 }
 
-void addPhotosToStorage(File file, String id) {
-  final ref = storage.ref("Posts").child(id).putFile(file);
+void addPhotosToStorage(Uint8List uint8list, String id) {
+  final ref = storage.ref("Posts").child(id).putData(uint8list);
+}
+
+Future<Uint8List> compressFile(File file) async {
+  var result = await FlutterImageCompress.compressWithFile(file.absolute.path,
+      minWidth: 1080, minHeight: 1080, quality: 50);
+  return result!;
 }
