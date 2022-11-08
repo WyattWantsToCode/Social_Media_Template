a3ba06c2944499285169a6b626ea392116b60c4a
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-11-08 10:52:27 -0600

    Created basic auth functions

---
+++ lib/UserProfile/auth.dart
@@ -0,0 +1,33 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:social_media_template/Posts/post_class.dart';
+import 'package:social_media_template/story_class.dart';
+import 'package:social_media_template/user_class.dart' as userClass;
+
+final auth = FirebaseAuth.instance;
+
+userClass.CurrentUser? currentUser;
+
+Future<User?> signInWithGoogle() async {
+  final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
+  final GoogleSignInAuthentication? googleAuth =
+      await googleUser?.authentication;
+  final credential = GoogleAuthProvider.credential(
+    accessToken: googleAuth?.accessToken,
+    idToken: googleAuth?.idToken,
+  );
+  await auth.signInWithCredential(credential);
+
+  return auth.currentUser;
+}
+
+void setCurrentUser(userClass.User user) {
+  currentUser = userClass.CurrentUser(
+      user: user,
+      likedPosts: <PostClass>[],
+      storiesWatched: <userClass.User>[]);
+}
+
+void updateAuthDisplayName(String name) {
+  auth.currentUser!.updateDisplayName(name);
+}
