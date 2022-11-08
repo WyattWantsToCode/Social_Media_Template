a66e067109e424235914e3e001addb69820e5da7
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-11-08 10:49:29 -0600

    Added Description TextStyle

--- lib/colors.dart
+++ lib/colors.dart
@@ -5,3 +5,4 @@ Color colorDarkGray = Color.fromRGBO(30, 30, 30, 1);
 Color colorIcon = Colors.white;
 TextStyle nameStyle = TextStyle(color: Colors.white, fontSize: 16);
 TextStyle styleDateTime = TextStyle(color: Colors.grey, fontSize: 12);
+TextStyle styleDescription = TextStyle(color: Colors.white, fontSize: 12);
