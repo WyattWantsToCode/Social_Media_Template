2f2c71632c9c588cfbfba5227f0efbaeca5cf0a7
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-11-08 10:53:04 -0600

    Created basic SignInPage

---
+++ lib/UserProfile/sign_in_page.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:social_media_template/Posts/post_class.dart';
+import 'package:social_media_template/UserProfile/auth.dart';
+import 'package:social_media_template/UserProfile/create_user_page.dart';
+import 'package:social_media_template/colors.dart';
+import 'package:social_media_template/firebase.dart';
+import 'package:social_media_template/home_page.dart';
+import 'package:social_media_template/story_class.dart';
+import 'package:social_media_template/user_class.dart';
+
+class SignInPage extends StatefulWidget {
+  SignInPage({Key? key}) : super(key: key);
+
+  @override
+  State<SignInPage> createState() => _SignInPageState();
+}
+
+class _SignInPageState extends State<SignInPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: colorBackground,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(50.0),
+          child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Center(
+                    child: Text(
+                  "Sign In please",
+                  style:
+                      GoogleFonts.pacifico(color: Colors.white, fontSize: 48),
+                )),
+                GestureDetector(
+                    onTap: () {
+                      signInWithGoogle().then((value) async {
+                        if (value != null) {
+                          if (await doesUserExist(value.uid)) {
+                            setCurrentUser(
+                                await getUserByHandle(value.displayName as String));
+                            Navigator.pushReplacement(context,
+                                MaterialPageRoute(builder: ((context) {
+                              return HomePage();
+                            })));
+                          } else {
+                            Navigator.pushReplacement(context,
+                                MaterialPageRoute(builder: ((context) {
+                              return CreateUserPage();
+                            })));
+                          }
+                        }
+                      });
+                    },
+                    child: Container(
+                      width: double.infinity,
+                      height: 75,
+                      decoration: BoxDecoration(color: colorDarkGray),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                          Text(
+                            "GOOGLE",
+                            style: GoogleFonts.roboto(
+                                color: Colors.white, fontSize: 36),
+                          ),
+                        ],
+                      ),
+                    ))
+              ]),
+        ),
+      ),
+    );
+  }
+}
