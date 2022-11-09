6f3addf54a262b20a454bb7eade2e91bdb8e45ed
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-11-09 10:46:11 -0600

    Updated Text Styles

--- lib/colors.dart
+++ lib/colors.dart
@@ -3,6 +3,6 @@ import 'package:flutter/material.dart';
 Color colorBackground = Color.fromRGBO(1, 1, 1, 1);
 Color colorDarkGray = Color.fromRGBO(30, 30, 30, 1);
 Color colorIcon = Colors.white;
-TextStyle nameStyle = TextStyle(color: Colors.white, fontSize: 16);
+TextStyle nameStyle = TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w700);
 TextStyle styleDateTime = TextStyle(color: Colors.grey, fontSize: 12);
-TextStyle styleDescription = TextStyle(color: Colors.white, fontSize: 12);
+TextStyle styleDescription = TextStyle(color: Colors.white, fontSize: 14);
