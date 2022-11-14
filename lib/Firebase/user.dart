06e8cf2be7bd443f5293bff008aa44d1e8c93dcb
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-11-14 12:36:38 -0600

    Added friend requesting functions

--- lib/Firebase/user.dart
+++ lib/Firebase/user.dart
@@ -36,12 +36,17 @@ Future<bool> isHanldleTaken(String handle) async {
 }
 
 User mapToUser(Map<String, dynamic> map, String id) {
+  List<String> list = <String>[];
+  for (var string in map["friendsIDs"]) {
+    list.add(string.toString());
+  }
   return User(
       id: id,
       displayName: map["displayName"],
       handle: map["handle"],
       profilePictureURL: map["profilePictureURL"],
-      authID: map["authID"]);
+      authID: map["authID"],
+      friendsIDs: list);
 }
 
 Future<void> addNewUser(User user) async {
@@ -56,17 +61,76 @@ Map<String, dynamic> userToMap(User user) {
     "handle": user.handle,
     "displayName": user.displayName,
     "profilePictureURL": user.profilePictureURL,
-    "authID": user.authID
+    "authID": user.authID,
+    "friendsIDs": user.friendsIDs
   };
-  if (user.followerList != null) {
-    map.addAll({"followerList": user.followerList});
-  }
-  if (user.followingList != null) {
-    map.addAll({"followingList": user.followingList});
-  }
   return map;
 }
 
 Future<void> removeUser(String id) async {
   await db.collection("Users").doc(id).delete();
+}
+
+void addUserIDToUserEXT(String id, Map<String, dynamic> map) async {
+  await db.collection("UsersEXT").doc(id).set(map);
+}
+
+Future<Map<String, dynamic>> getUserEXTfromID(String id) async {
+  Map<String, dynamic> map = {};
+  final ref = await db.collection("UsersEXT").doc(id);
+  await ref.get().then((value) {
+    if (value != null) {
+      map.addAll(value.data()!);
+    }
+  });
+  return map;
+}
+
+void sendFriendRequest(String senderID, String receiverID) async {
+  Map<String, dynamic> userMap = await getUserEXTfromID(receiverID);
+  Map<String, dynamic> friendRequestMap = {};
+  if (userMap['friendRequests'] != null) {
+    friendRequestMap.addAll(userMap['friendRequests']);
+  }
+  friendRequestMap[senderID] = Timestamp.now();
+  addUserIDToUserEXT(receiverID, {"friendRequests": friendRequestMap});
+}
+
+void removeFriendRequest(String userID, String newFriend) async {
+  Map<String, dynamic> userMap = await getUserEXTfromID(userID);
+  print(userMap);
+  Map<String, dynamic> friendRequestMap = userMap["friendRequests"];
+  print(friendRequestMap);
+  userMap.remove("friendRequests");
+  print(userMap);
+  friendRequestMap.remove(newFriend);
+  print(friendRequestMap);
+  userMap.addAll(friendRequestMap);
+  print(userMap);
+  addUserIDToUserEXT(userID, userMap);
+}
+
+Future<List<String>> getFriendRequests(String id) async {
+  Map<String, dynamic> map = await getUserEXTfromID(id);
+  List<FreindRequest> friendList = <FreindRequest>[];
+  List<String> list = <String>[];
+  if (map["friendRequests"] != null) {
+    (map["friendRequests"]).forEach((key, value) {
+      friendList.add(FreindRequest(friendID: key, timestamp: value));
+    });
+    friendList.sort(
+      (FreindRequest a, FreindRequest b) => a.timestamp.compareTo(b.timestamp),
+    );
+    for (FreindRequest request in friendList) {
+      list.add(request.friendID);
+    }
+  }
+  return list;
+}
+
+class FreindRequest {
+  String friendID;
+  Timestamp timestamp;
+
+  FreindRequest({required this.friendID, required this.timestamp});
 }
