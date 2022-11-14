938e87fc3f7d3e2323916dd722fceb19d5c83de0
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-11-14 12:36:16 -0600

    Removed story_class package

--- lib/Firebase/firebase.dart
+++ lib/Firebase/firebase.dart
@@ -3,8 +3,7 @@ import 'dart:math';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:social_media_template/Posts/post_class.dart';
-import 'package:social_media_template/story_class.dart';
-import 'package:social_media_template/user_class.dart';
+
 
 var db = FirebaseFirestore.instance;
 
