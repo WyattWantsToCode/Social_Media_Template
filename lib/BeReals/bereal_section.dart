fc98c6c4e3de52da9141162cd697a72a5241d24d
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-11-17 16:50:48 -0600

    Updated BeRealButton

--- lib/BeReals/bereal_section.dart
+++ lib/BeReals/bereal_section.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:social_media_template/BeReals/bereals_page.dart';
+import 'package:social_media_template/BeReals/post_bereal_page.dart';
+import 'package:social_media_template/BeReals/take_bereal_picture_page.dart';
 import 'package:social_media_template/Firebase/storage.dart';
 import 'package:social_media_template/UserProfile/auth.dart';
 import 'package:social_media_template/colors.dart';
@@ -40,12 +43,25 @@ class BeRealSection extends StatelessWidget {
           height: 108,
           child: Row(
             children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 15),
-                child: BeRealButton(
-                  user: user,
-                  ringOn: false,
-                ),
+              Stack(
+                children: [
+                  
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 15),
+                    child: BeRealButton(
+                      user: user,
+                      ringOn: false,
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.push(context, MaterialPageRoute(builder: (context) {
+                    return TakeBeRealPage();
+                  }));
+                    },
+                    child: Container(width: 100, height: 100, color: Colors.transparent,),),
+                  
+                ],
               ),
               Expanded(
                 child: ListView.builder(
@@ -84,12 +100,14 @@ class BeRealButton extends StatefulWidget {
     this.scale = 1,
     required this.ringOn,
     this.nameUnderOne = true,
+
   }) : super(key: key);
   User user;
   Function? onPressedFunction;
   double scale;
   bool ringOn;
   bool nameUnderOne;
+
 
   @override
   State<BeRealButton> createState() => _BeRealButtonState();
@@ -98,80 +116,93 @@ class BeRealButton extends StatefulWidget {
 class _BeRealButtonState extends State<BeRealButton> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Stack(
-          alignment: Alignment.center,
-          children: [
-            widget.ringOn
-                ? Opacity(
-                    opacity: widget.scale < .7? 1 : .9,
-                    child: Container(
-                      width: widget.scale < .7? 80 * widget.scale + 1 : 80 * widget.scale,
-                      height:  widget.scale < .7? 80 * widget.scale + 1 : 80 * widget.scale,
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                            begin: Alignment.topRight,
-                            end: Alignment.bottomLeft,
-                            colors: [
-                              Colors.purple,
-                              Colors.purple,
-                              Colors.pink,
-                              Colors.red,
-                              Colors.orange,
-                              Colors.yellow,
-                            ]),
-                        shape: BoxShape.circle,
-                      ),
-                    ),
-                  )
-                : Container(),
-            Container(
-              width: 76 * widget.scale,
-              height: 76 * widget.scale,
-              decoration: BoxDecoration(
-                color: colorBackground,
-                shape: BoxShape.circle,
-              ),
-            ),
-            FutureBuilder(
-              future: getProfilePictureURL(widget.user.profilePictureURL),
-              builder: ((context, snapshot) {
-                if (snapshot.connectionState == ConnectionState.done) {
-                  if (snapshot.hasError) {
-                    return const Center(
-                      child: Text("Error"),
-                    );
-                  } else if (snapshot.hasData) {
-                    return Container(
-                      width: 70 * widget.scale,
-                      height: 70 * widget.scale,
-                      decoration: BoxDecoration(
-                          border:
-                              Border.all(color: Colors.white54, width: .5),
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) {
+          return BeRealsPage();
+        }));
+      },
+      child: Column(
+        children: [
+          Stack(
+            alignment: Alignment.center,
+            children: [
+              widget.ringOn
+                  ? Opacity(
+                      opacity: widget.scale < .7 ? 1 : .9,
+                      child: Container(
+                        width: widget.scale < .7
+                            ? 80 * widget.scale + 1
+                            : 80 * widget.scale,
+                        height: widget.scale < .7
+                            ? 80 * widget.scale + 1
+                            : 80 * widget.scale,
+                        decoration: BoxDecoration(
+                          gradient: LinearGradient(
+                              begin: Alignment.topRight,
+                              end: Alignment.bottomLeft,
+                              colors: [
+                                Colors.purple,
+                                Colors.purple,
+                                Colors.pink,
+                                Colors.red,
+                                Colors.orange,
+                                Colors.yellow,
+                              ]),
                           shape: BoxShape.circle,
-                          image: DecorationImage(
-                              image: NetworkImage(snapshot.data as String),
-                              fit: BoxFit.cover)),
-                    );
+                        ),
+                      ),
+                    )
+                  : Container(),
+              Container(
+                width: 76 * widget.scale,
+                height: 76 * widget.scale,
+                decoration: BoxDecoration(
+                  color: colorBackground,
+                  shape: BoxShape.circle,
+                ),
+              ),
+              FutureBuilder(
+                future: getProfilePictureURL(widget.user.profilePictureURL),
+                builder: ((context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    if (snapshot.hasError) {
+                      return const Center(
+                        child: Text("Error"),
+                      );
+                    } else if (snapshot.hasData) {
+                      return Container(
+                        width: 70 * widget.scale,
+                        height: 70 * widget.scale,
+                        decoration: BoxDecoration(
+                            border:
+                                Border.all(color: Colors.white54, width: .5),
+                            shape: BoxShape.circle,
+                            image: DecorationImage(
+                                image: NetworkImage(snapshot.data as String),
+                                fit: BoxFit.cover)),
+                      );
+                    }
                   }
-                }
 
-                return const Center(
-                  child: CircularProgressIndicator(),
-                );
-              }),
-            )
-          ],
-        ),
-        widget.nameUnderOne? Padding(
-          padding: const EdgeInsets.only(top: 5),
-          child: Text(
-            widget.user.handle,
-            style: nameStyle.apply(fontSizeFactor: .75),
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }),
+              )
+            ],
           ),
-        ): Container(),
-      ],
+          widget.nameUnderOne
+              ? Padding(
+                  padding: const EdgeInsets.only(top: 5),
+                  child: Text(
+                    widget.user.handle,
+                    style: nameStyle.apply(fontSizeFactor: .75),
+                  ),
+                )
+              : Container(),
+        ],
+      ),
     );
   }
 }
