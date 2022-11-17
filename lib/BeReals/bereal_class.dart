57cfc03d0a93f4830eafd4a2233289b0b4a8ef7c
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-11-17 16:49:39 -0600

    Added timestamp to BeReal class

--- lib/BeReals/bereal_class.dart
+++ lib/BeReals/bereal_class.dart
@@ -1,7 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+
 class BeReal {
   String id;
   List<String> photoURLs;
   String user;
+  Timestamp timestamp;
 
-  BeReal({required this.id, required this.photoURLs, required this.user});
+  BeReal({required this.id, required this.photoURLs, required this.user, required this.timestamp});
 }
