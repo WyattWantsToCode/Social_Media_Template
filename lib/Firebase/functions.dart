4b1c96bbc00e606a0f13b06fdb1cd053cbdec75b
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-11-17 16:52:29 -0600

    Removed function calling

--- lib/Firebase/functions.dart
+++ lib/Firebase/functions.dart
@@ -2,17 +2,16 @@ import 'dart:convert';
 
 import 'package:http/http.dart';
 
-import 'package:cloud_functions/cloud_functions.dart';
+
 import 'package:social_media_template/Firebase/post.dart';
 import 'package:social_media_template/Posts/post_class.dart';
 
-final functions = FirebaseFunctions.instance;
 
-Future<String> functionCall(String postID) async {
-  final URL = Uri.parse(
-      "https://us-central1-social-media-template-4f6b1.cloudfunctions.net/readPostByID?id=" +
-          postID);
-  Response response = await get(URL);
-  Map map = jsonDecode(response.body);
-  return map["value"];
-}
+//Future<String> functionCall(String postID) async {
+  //final URL = Uri.parse(
+      //"https://us-central1-social-media-template-4f6b1.cloudfunctions.net/readPostByID?id=" +
+          //postID);
+  //Response response = await get(URL);
+  //Map map = jsonDecode(response.body);
+  //return map["value"];
+//}
