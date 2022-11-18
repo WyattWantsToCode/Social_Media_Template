33a7a24126783dc643ac022a14f1c47af3b195a6
Wyatt Vaughan <w.vaughan.2021@gmail.com>
2022-11-18 16:25:10 -0600

    Added different colors for different pages

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-Color colorBackground = Color.fromRGBO(1, 1, 1, 1);
+final Color colorBackground = Color.fromRGBO(1, 1, 1, 1);
 Color colorDarkGray = Color.fromRGBO(30, 30, 30, 1);
+Color colorProfile = Colors.blue;
+Color colorChallenge = Colors.purple;
+Color colorBeReal = Colors.red;
+Color colorPost = Colors.orange;
 Color colorIcon = Colors.white;
-TextStyle nameStyle = TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w700);
+TextStyle nameStyle =
+    TextStyle(color: Colors.white, fontSize: 16, fontWeight: FontWeight.w700);
 TextStyle styleDateTime = TextStyle(color: Colors.grey, fontSize: 12);
 TextStyle styleDescription = TextStyle(color: Colors.white, fontSize: 14);
+TextStyle stylePrompt = GoogleFonts.quicksand(textStyle: TextStyle(color: Colors.white, fontSize: 32));
